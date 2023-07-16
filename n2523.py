7f19dec351e9ba6f1d49ef673f37bdaf76e6b93d
2UJIN <83401978+2UJ1N@users.noreply.github.com>
2023-07-16 23:44:17 +0900

    과제

---
+++ n2523.py
@@ -0,0 +1,10 @@
+#
+# 2501
+# 인공지능 시계
+# https://www.acmicpc.net/problem/2501
+
+import sys
+input = sys.stdin.readline
+
+h, m, s = map(int, input().split())
+cook = int(input())
---
+++ n3554.py
@@ -0,0 +1,10 @@
+#
+# 3004
+# 네 번째 수
+# https://www.acmicpc.net/problem/3004
+
+import sys
+input = sys.stdin.readline
+
+h, m, s = map(int, input().split())
+cook = int(input())
